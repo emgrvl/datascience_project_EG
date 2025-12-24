74e31c8e6b8650505d9dcb902cfc26431ec16a1c
Emeline <emeline.gravaillac@gmail.com>
2025-12-24 14:07:50 +0000

    Update load_data

--- scr/data/load_data.py
+++ scr/data/load_data.py
@@ -1,50 +1,65 @@
+import re
 import pandas as pd
-from pubchempy import get_compounds
-
-#Retrieve PubChem Data
-def fetch_pubchem(smiles_list: list[str]) -> pd.DataFrame:
-    """
-        Fetch chemical properties from PubChem API.
-        Args:
-            smiles_list (list[str]): List of SMILES strings.
-        Returns:
-            pd.DataFrame: DataFrame with additional molecular info.
-        """
-    #PubChem API query
-
-    pubchem_data = []
-    for cas_number in smiles_list:
-        try:
-            compound = get_compounds(cas_number, 'name')[0]
-            smiles = compound.canonical_smiles
-            molecular_formula = compound.molecular_formula
-            exact_mass = compound.exact_mass
-            inchi_key = compound.inchi_key
-            print(f"CAS Number: {cas_number}, SMILES: {smiles}, Molecular Formula: {molecular_formula}, Exact Mass: {exact_mass}, InChIKey: {inchi_key}")
-            pubchem_data.append({
-                'CAS_Number': cas_number,
-                'SMILES': smiles,
-                'Molecular_Formula': molecular_formula,
-                'Exact_Mass': exact_mass,
-                'InChIKey': inchi_key
-            })
-
-        except Exception as e:
-            print(f"Error fetching data for CAS number {cas_number}: {e}")
-
-    pubchem_df = pd.DataFrame(pubchem_data)
-    return pubchem_df
+from pubchempy import get_compounds, get_synonyms
 
 
 #Load FIG
-import pandas as pd
-
-def load_ifra_fig(file_path = 'raw_ifra-fig.csv') -> pd.DataFrame:
+def load_ifra_fig(file_path = '/files/datascience_project_EG/data/raw_ifra-fig.csv') -> pd.DataFrame:
     raw_fig = pd.read_csv(file_path, sep=';')
     raw_fig.head()
+    print(raw_fig.columns)
+    print(f"FIG dataset contains {len(raw_fig)} molecules.")
+    print(raw_fig.head())
     return raw_fig
 
-#merge FIG dataset with pubchem SMILES strings
+
+#CAS Registry Numbers are not officially supported by PubChem, 
+# but they are often present in the synonyms associated with a compound. 
+# Therefore it is straightforward to retrieve them by filtering the synonyms 
+# to just those with the CAS Registry Number format:
+#le pb c'est que certaines molecules dans pubchem sont formées à partir d'autres donc on peut
+#faire face à des doublons ou des absences, le but est donc de lié fig et pubchem par le nom principal et vérifier
+#si le cas number est bien le meme entre les deux bases de données
+
+#Retrieve PubChem Data with CAS Numbers of FIG dataset
+def fetch_pubchem(fig: pd.DataFrame) -> pd.DataFrame:
+    smiles_list = []
+    cas_rns = []
+    compounds_list = []
+
+    for index, molecule in fig.iterrows(): #on itère sur les elt de fig
+        #print(molecule['CAS number'])
+        results = get_synonyms(molecule['CAS number'], "name") #PB le nom principal n'est pas forcément le meme entre les deux bases meme si on retrouve celui du fig dans les synonymes de pubchem
+        #results correspond à la liste de tous les noms associés à la molécule
+        for result in results: 
+            #cid = result["CID"] #this is a unique identifier for each compound in PubChem
+            for syn in result.get("Synonym", []): #dans le cas de plusieurs syn
+               match = re.match(r"(\d{2,7}-\d\d-\d)", syn) 
+               if match and match.group(1) == molecule['CAS number']:
+                   cas_rns.append(match.group(1))
+                   #print(f"Found CAS {match.group(1)} for molecule {molecule['Principal name']}")
+                   compounds = get_compounds(syn, 'name') #molecule from pubchem to get SMILES 
+                   if compounds:
+                        smiles_list.append(compounds[0].connectivity_smiles) #canonical_smiles is deprecated: Use connectivity_smiles instead
+                        compounds_list.append(compounds) #toutes les infos de la molécule sont dans compounds ATTENTION ICI
+                   else:
+                        smiles_list.append(None)
+                   break
+    chem_df = pd.DataFrame({'CAS number': cas_rns, 'SMILES': smiles_list, 'Compounds': compounds_list})
+    print(f"Retrieved {len(chem_df)} molecules from PubChem.")
+    chem_df.head()
+    return chem_df
+
+#Simplified Molecular Input Line Entry System (SMILES)
+
+#merge FIG dataset with pubchem SMILES strings and other info
 def merge_smiles(df_fig: pd.DataFrame, df_pubchem: pd.DataFrame) -> pd.DataFrame:
-    merged_df = pd.merge(df_fig, df_pubchem, on='CAS_Number', how='left')
+    merged_df = pd.merge(df_fig, df_pubchem, on='CAS number', how='left')
     return merged_df
+
+
+if __name__ == "__main__":
+    raw_fig = load_ifra_fig()
+    pubchem_df = fetch_pubchem(raw_fig)
+    fig_with_pubchem = merge_smiles(raw_fig, pubchem_df)
+    fig_with_pubchem.to_csv('/files/datascience_project_EG/data/fig_with_pubchem.csv', index=False) #save the merged dataset to a csv file
